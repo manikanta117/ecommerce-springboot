e202bcd62df619bd97825f29a8858fc88400c5de
manikanta117 <manikantaorsu7@gmail.com>
2026-02-01 09:18:13 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/eco/ECommersWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.eco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommersWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommersWebsiteApplication.class, args);
+	}
+
+}
